516a26ed28644f80e610eb5e6267d052284cd623
Mike Ball <mikedball@gmail.com>
2015-12-11 08:15:24 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+import express from 'express'
+import http from 'http'
+import bodyParser from 'body-parser'
+import Instagram from 'instagram-node-lib'
+import dotenv from 'dotenv'
+import Twilio from 'twilio'
+
+dotenv.load();
+
+let twilio = new Twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+let port = process.env.PORT || 3000;
+let app = express();
+let server = http.Server(app);
+
+Instagram.set('client_id', process.env.IG_CLIENT_ID);
+Instagram.set('client_secret', process.env.IG_CLIENT_SECRET);
+
+Instagram.users.subscribe({
+  object: 'user',
+  object_id: process.env.IG_USER_ID,
+  aspect: 'media',
+  callback_url: process.env.CALLBACK_URL,
+  type: 'subscription'
+});
+
+app.use(bodyParser.json());
+
+app.get('/alert', (req, res) => {
+  Instagram.subscriptions.handshake(req, res);
+});
+
+app.post('/alert', (req, res) => {
+  res.send();
+  Instagram.users.recent({
+    user_id: req.body[0].object_id,
+    complete: (data) => {
+      console.log(data);
+
+      var username = data[0].user.username,
+          link = data[0].link;
+
+      twilio.sendMessage({
+        to: process.env.PHONE_NUMBER,
+        from: process.env.TWILIO_PHONE_NUMBER,
+        body: 'New post from ' + username + ':\n' + link
+      }, function(err, response) {
+        if (err) console.log(err);
+
+        console.log(response);
+      });
+    }
+  });
+});
+
+server.listen(port);
+
+console.log('Listening ...');
